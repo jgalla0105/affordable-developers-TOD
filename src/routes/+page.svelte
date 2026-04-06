a03eec63e0d8c1ab65a4d36967080d5b42ac01f5
eriolay <126813646+eriolay@users.noreply.github.com>
2026-04-06 17:34:18 -0400

    Functional WordCloud
    got wordcloud working, still need to clean up code and do styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script> 
+import WordCloud from '$lib/WordCloud.svelte';
+
+</script>
+
+<h1>What is TOD?</h1>
+<p>**insert some text explaining what TOD is**</p>
+<h4>Okay, but why should I care?</h4>
+<p>Because look how MAPC communities are improving to promote TOD!</p>
+<WordCloud csvURL='../static/wordcloud-classified-data.csv' />
+
+
+<style>
+
+</style>
