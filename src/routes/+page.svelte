2878b8e73194dad0d1b0102196b9266222e4f043
eriolay <126813646+eriolay@users.noreply.github.com>
2026-04-07 22:15:21 -0400

    added dotgrid font for headings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@ import WordCloud from '$lib/WordCloud.svelte';
 
 </script>
 
-<h1>What is TOD?</h1>
+<h1>WHAT IS TOD?</h1>
 <p>**insert some text explaining what TOD is**</p>
 <h4>Okay, but why should I care?</h4>
 <p>Because look how MAPC communities are improving to promote TOD!</p>
@@ -11,5 +11,20 @@ import WordCloud from '$lib/WordCloud.svelte';
 
 
 <style>
+    @font-face {
+        font-family: 'DotFont';
+        src: url('/DOTMATRI.TTF') format('truetype');
+        font-weight: normal;
+        font-style: normal;
+        font-display: swap;
+    }
+
+    h1 {
+        font-family: 'DotFont', sans-serif;
+        font-size: 60px;
+    }
+    h1, p, h2, h3, h4 {
+        color:rgb(59, 59, 59);
+    }
 
 </style>
